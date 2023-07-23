c1e6620974aeb8e41ea4c12fc3d7f170bf6cfdd1
nikolay <kndev@mac.local>
2023-07-23 12:54:29 +0300

    obfuscate

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import * as path from "path";
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
     plugins: [
         // генерация файла `index.d.ts`
         dts({
             insertTypesEntry: true,
+        }),
+        obfuscatorPlugin({
+            exclude: [/node_modules/],
+            apply: "build",
+            debugger: true,
         }),
     ],
     build: {
