a9460ddae1d0eb8a92afbd44de8f7f6e1828dac7
nikolay <kndev@mac.local>
2023-07-24 00:39:46 +0300

    add sample

--- src/app/main.ts
+++ src/app/main.ts
@@ -12,15 +12,30 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Animate writer</h1>
+    
+    <h2 style="max-width: 100%; overflow: hidden;white-space: nowrap;text-overflow: ellipsis">
+    <span style="text-decoration: underline; font-weight: 300">With white-space nowrap</span><br>
+        &#8203;<span class="js-animate-write-1" data-aw-speed="150"></span>
+    </h2>
+    
     <h2>
-        &#8203;<span class="js-animate-write" data-aw-speed="150"></span>
+    <span style="text-decoration: underline; font-weight: 300">With white-space normal</span><br>
+        &#8203;<span class="js-animate-write-2" data-aw-speed="150"></span>
     </h2>
   </div>
 `
 
-initAnimateWriter('.js-animate-write', {
+initAnimateWriter('.js-animate-write-1', {
     input: [
-        'Lorem Ipsum is simply dummy text',
+        'Lorem Ipsum is simply dummy text. It has survived not only five centuries. It was popularised in the 1960s',
+        'It has survived not only five centuries',
+        'It was popularised in the 1960s'
+    ],
+});
+
+initAnimateWriter('.js-animate-write-2', {
+    input: [
+        'Lorem Ipsum is simply dummy text. It has survived not only five centuries. It was popularised in the 1960s',
         'It has survived not only five centuries',
         'It was popularised in the 1960s'
     ],
